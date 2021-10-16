770c0accaca79e9c229d093e26b1581b679d6a34
buraiyen <brian.edison.nguyen@gmail.com>
2021-10-16 11:52:18 -0700

    s3: dynamic data

--- S3_React_Basics_and_Components/react-complete-guide/src/components/ExpenseItem.js
+++ S3_React_Basics_and_Components/react-complete-guide/src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
-	return (
-		<div className='expense-item'>
-			<div>March 28th 2021</div>
-			<div className='expense-item__description'>
-				<h2>Car Insurance</h2>
-				<div className='expense-item__price'>$297.48</div>
-			</div>
-		</div>
-	);
+  // Note: months start at 0
+  // Date object is not a string
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 297.48;
+  return (
+    <div className='expense-item'>
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
