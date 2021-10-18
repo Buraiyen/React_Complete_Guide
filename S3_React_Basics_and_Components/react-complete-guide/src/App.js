02b4ee72e91f0e712d9d310ebe63132d6e7fdc6a
buraiyen <brian.edison.nguyen@gmail.com>
2021-10-17 20:15:42 -0700

    s3: closer look at JSX

--- S3_React_Basics_and_Components/react-complete-guide/src/App.js
+++ S3_React_Basics_and_Components/react-complete-guide/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./components/Expenses";
 
 function App() {
@@ -22,12 +23,18 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses expenses={expenses} />
-    </div>
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { expenses: expenses })
   );
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses expenses={expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
