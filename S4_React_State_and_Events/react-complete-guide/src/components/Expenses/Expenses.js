4cc31c28f116d48eb89835c7a1c52bf2efbfde22
buraiyen <brian.edison.nguyen@gmail.com>
2021-10-20 11:35:37 -0700

    import react project

---
+++ S4_React_State_and_Events/react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      {props.expenses.map(function (obj, i) {
+        return (
+          <ExpenseItem
+            title={props.expenses[i].title}
+            amount={props.expenses[i].amount}
+            date={props.expenses[i].date}
+          />
+        );
+      })}
+    </Card>
+  );
+};
+
+export default Expenses;
