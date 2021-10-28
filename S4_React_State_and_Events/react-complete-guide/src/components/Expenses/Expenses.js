e02559c5b61b1be25aec83c42233b0d77f9679f6
buraiyen <brian.edison.nguyen@gmail.com>
2021-10-28 11:53:39 -0700

    s4: assignment

--- S4_React_State_and_Events/react-complete-guide/src/components/Expenses/Expenses.js
+++ S4_React_State_and_Events/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,20 +1,35 @@
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import "./Expenses.css";
+import { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
+import Card from '../UI/Card';
+import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {props.expenses.map(function (obj, i) {
-        return (
-          <ExpenseItem
-            title={props.expenses[i].title}
-            amount={props.expenses[i].amount}
-            date={props.expenses[i].date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        {props.expenses.map(function (obj, i) {
+          return (
+            <ExpenseItem
+              title={props.expenses[i].title}
+              amount={props.expenses[i].amount}
+              date={props.expenses[i].date}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
