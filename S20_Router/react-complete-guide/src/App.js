9a490b70169167699f0bda52c1c86fd23a6be920
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 12:11:46 -0800

    s20: working with links

--- S20_Router/react-complete-guide/src/App.js
+++ S20_Router/react-complete-guide/src/App.js
@@ -1,12 +1,14 @@
 // App.js
 import { Route } from 'react-router-dom';
 
-import Welcome from './components/Welcome';
-import Products from './components/Products';
+import Welcome from './views/Welcome';
+import Products from './views/Products';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path='/welcome'>
         <Welcome />
       </Route>
