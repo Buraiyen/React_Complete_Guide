91889a85cf98bb26e155eab4bb1f8c3d927c33db
buraiyen <brian.edison.nguyen@gmail.com>
2021-12-05 12:52:57 -0800

    s20: dynamic paths

--- S20_Router/react-complete-guide/src/App.js
+++ S20_Router/react-complete-guide/src/App.js
@@ -3,18 +3,24 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './views/Welcome';
 import Products from './views/Products';
+import ProductDetail from './views/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
   return (
     <div>
       <MainHeader />
-      <Route path='/welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/products'>
-        <Products />
-      </Route>
+      <main>
+        <Route path='/welcome'>
+          <Welcome />
+        </Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+        <Route path='/product-detail/:productId'>
+          <ProductDetail />
+        </Route>
+      </main>
     </div>
   );
 }
