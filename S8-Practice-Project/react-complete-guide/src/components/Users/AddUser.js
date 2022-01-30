7ab4b8b553a32d4906c8312a5dc94c2971198c46
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-30 12:37:05 -0800

    s8: reusable card component

--- S8-Practice-Project/react-complete-guide/src/components/Users/AddUser.js
+++ S8-Practice-Project/react-complete-guide/src/components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from '../UI/Card';
+import styles from './AddUser.module.css';
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input type='text' id='username' />
-      <label htmlFor='username'>Age (Years)</label>
-      <input type='username' id='number' />
-      <button type='submit'>Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' id='username' />
+        <label htmlFor='username'>Age (Years)</label>
+        <input type='username' id='number' />
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
   );
 };
 
