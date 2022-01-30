29087314aa872eb5db353a1e88dc8236ad2c3a5d
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-30 15:27:29 -0800

    s8: validation and resetting logic

--- S8-Practice-Project/react-complete-guide/src/components/Users/AddUser.js
+++ S8-Practice-Project/react-complete-guide/src/components/Users/AddUser.js
@@ -9,7 +9,15 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (!enteredUsername.trim().length || !enteredAge.trim.length) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -26,9 +34,19 @@ const AddUser = (props) => {
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' onChange={usernameChangeHandler} />
+        <input
+          type='text'
+          id='username'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='username'>Age (Years)</label>
-        <input type='number' id='number' onChange={ageChangeHandler} />
+        <input
+          type='number'
+          id='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
