5d76c83535023cc9d94b4b8fbf7379160bb3670a
buraiyen <brian.edison.nguyen@gmail.com>
2022-02-03 11:54:37 -0800

    s9: wrapper

--- S9-Diving-Deeper/react-complete-guide/src/components/Users/AddUser.js
+++ S9-Diving-Deeper/react-complete-guide/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
@@ -46,7 +47,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -73,7 +74,7 @@ const AddUser = (props) => {
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
-    </>
+    </Wrapper>
   );
 };
 
